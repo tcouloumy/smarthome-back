dfd8d990331595d9fdb88ac2c8fb82cfe9d38863
Thibault <t.couloumy@gmail.com>
2020-03-25 18:42:28 +0100

    Redo set color controller function

--- modules/yeelight/yeelight.controller.js
+++ modules/yeelight/yeelight.controller.js
@@ -26,8 +26,8 @@ module.exports = function YeelightController() {
 
 	function setBrightness(req, res) {
 		
-		let brightness = parseInt(req.body.brightness);
 		let light = YeelightService.getLightByUid(req.params.uid);
+		let brightness = parseInt(req.body.brightness);
 
 		pt.timeout(light.set_bright(brightness), 2000).then((data) => {
 			return res.send(JSON.parse(data));
@@ -46,28 +46,27 @@ module.exports = function YeelightController() {
 
 	function setColor(req, res) {
 
-		// const yeelight = new y.Yeelight({
-		// 	lightIp: '192.168.0.12',
-		// 	lightPort: '55443'
-		// });
+		let light = YeelightService.getLightByUid(req.params.uid);
+		let r = parseInt(req.body.r),
+			g = parseInt(req.body.g),
+			b = parseInt(req.body.b);
 
-		// let r = parseInt(req.body.r),
-		// 	g = parseInt(req.body.g),
-		// 	b = parseInt(req.body.b);
-
-		// yeelight.connect().then(() => {
 			
-		// 	yeelight.setRGB(new y.Color(r, g, b), "smooth", 1000);
-			
-		// 	res.status(200).json({
-		// 		status: 'ok'
-		// 	});
-		// })
-		// .catch((error) => {
-		// 	res.json({
-		// 		error: error
-		// 	});
-		// });
+			// pt.timeout(light.setRGB(new Yeelight.Color(r, g, b), "smooth", 1000), 2000).then((data) => {
+			pt.timeout(light.set_rgb([r, g, b], "smooth", 1000), 2000).then((data) => {
+				return res.send(JSON.parse(data));
+			})
+			.catch((err) => {
+				// TODO : a helper error handler
+				if (err instanceof pt.TimeoutError) {
+					console.log(err);
+					return res.status(504).send(err);
+				}
+				else {
+					return res.status(500).json(err);
+				}
+			})
+		
 	}
 
 	return Object.freeze({
